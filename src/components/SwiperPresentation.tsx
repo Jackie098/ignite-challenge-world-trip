bf20807d50887c6f4bce3f7b611f005db955a90c
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-07-27 22:14:09 -0300

    refactor: deleted unused code

--- src/components/SwiperPresentation.tsx
+++ src/components/SwiperPresentation.tsx
@@ -28,6 +28,8 @@ interface SwiperPresentationProps {
 export function SwiperPresentation({
   queryData: { isFetching, data: continents },
 }: SwiperPresentationProps) {
+  console.log("continents - comp", continents);
+
   return (
     <VStack mt={"52px"}>
       <Box color={"darkText.500"} fontSize={"36px"}>
@@ -40,60 +42,62 @@ export function SwiperPresentation({
       </Box>
 
       <Flex w={"80vw"} h={"450px"} mt={"52px"} mb={"48px"}>
-        {isFetching ? (
+        {/* {isFetching ? (
           <Flex w={"100%"} align={"center"} justify={"center"}>
             <Spinner color="dartText.500" size={"lg"} />
           </Flex>
-        ) : (
-          <Swiper
-            modules={[Navigation, Pagination]}
-            navigation={{}}
-            pagination={{ clickable: true }}
-            spaceBetween={50}
-            autoplay={{
-              delay: 3500,
-              pauseOnMouseEnter: true,
-            }}
-            // onSlideChange={() => console.log("slide change")}
-            // onSwiper={(swiper) => console.log(swiper)}
-          >
-            {continents!.map((continent) => (
-              <SwiperSlide key={continent.alias}>
-                <Box h={"100%"} background={"blackAlpha.900"}>
-                  <Link href={continent.alias}>
-                    <VStack
-                      h={"100%"}
-                      justify={"center"}
-                      bgImage={`url(${continent.presentation_banner})`}
-                      bgRepeat={"no-repeat"}
-                      bgSize={"cover"}
-                      bgPosition={"center"}
-                    >
-                      <Box w={"100%"} h={"100%"} background={"black.50"}></Box>
-                      <Box position={"absolute"} zIndex={20}>
-                        <Text
-                          fontWeight={"bold"}
-                          fontSize={48}
-                          color={"lightText.900"}
-                          align={"center"}
-                        >
-                          {continent.continent_name}
-                        </Text>
-                        <Text
-                          fontSize={24}
-                          color={"lightText.900"}
-                          align={"center"}
-                        >
-                          {continent.decoy}
-                        </Text>
-                      </Box>
-                    </VStack>
-                  </Link>
-                </Box>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
+        ) : ( */}
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation={{}}
+          pagination={{ clickable: true }}
+          spaceBetween={50}
+          autoplay={{
+            delay: 3500,
+            pauseOnMouseEnter: true,
+          }}
+          // onSlideChange={() => console.log("slide change")}
+          // onSwiper={(swiper) => console.log(swiper)}
+        >
+          {continents!.map((continent) => (
+            <SwiperSlide key={continent.alias}>
+              <Box h={"100%"} background={"blackAlpha.900"}>
+                <Link href={continent.alias}>
+                  <VStack
+                    h={"100%"}
+                    justify={"center"}
+                    bgImage={`url(${continent.presentation_banner})`}
+                    bgRepeat={"no-repeat"}
+                    bgSize={"cover"}
+                    bgPosition={"center"}
+                  >
+                    <Box w={"100%"} h={"100%"} background={"black.50"}></Box>
+                    <Box position={"absolute"} zIndex={20}>
+                      <Text
+                        fontWeight={"bold"}
+                        fontSize={48}
+                        color={"lightText.900"}
+                        align={"center"}
+                      >
+                        {continent.continent_name}
+                      </Text>
+                      <Text
+                        fontSize={24}
+                        color={"lightText.900"}
+                        align={"center"}
+                      >
+                        {continent.decoy}
+                      </Text>
+                    </Box>
+                  </VStack>
+                </Link>
+              </Box>
+            </SwiperSlide>
+          ))}
+          {/* <SwiperSlide>2</SwiperSlide> */}
+        </Swiper>
+        {/* ) */}
+        {/* } */}
       </Flex>
     </VStack>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,10 @@
-import {
-  Box,
-  Divider,
-  Flex,
-  HStack,
-  Spinner,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
-import Image from "next/image";
+import { Box, Divider, Flex, Spinner, Text } from "@chakra-ui/react";
 
 import { useSortedContinents } from "@/services/hooks/useContinents";
-import Link from "next/link";
 
+import { Decoy } from "@/components/Decoy";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
-import { Decoy } from "@/components/Decoy";
 import { SwiperPresentation } from "@/components/SwiperPresentation";
 
 // CHECKPOINT
@@ -22,34 +12,12 @@ import { SwiperPresentation } from "@/components/SwiperPresentation";
 // TODO: : Add responsiviness
 // TODO: : Fetch API
 
-// type City = {
-//   city_name: string;
-//   country: string;
-//   iso_code: string;
-//   city_image: string;
+// type Continent = {
+//   continent_name: string;
+//   alias: string;
+//   decoy: string;
+//   presentation_banner: string;
 // };
-
-type Continent = {
-  continent_name: string;
-  alias: string;
-  decoy: string;
-  presentation_banner: string;
-  // description: string;
-  // page_banner: string;
-  // amount_countries: string;
-  // amount_languages: string;
-  // amount_cities_100: string;
-  // examples_cities_100: City[];
-};
-
-// type PreviewContinent = Pick<
-//   Continent,
-//   "continent_name" | "alias" | "decoy" | "presentation_banner"
-// >;
-
-// interface HomeProps {
-//   continents: Continent[];
-// }
 
 export default function Home() {
   const { data: continents, status, isFetching } = useSortedContinents();
